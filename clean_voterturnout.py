78545b07e98d568252e760c35859b8c797290288
StochajK <stochaj.k@northeastern.edu>
2022-11-03 21:11:57 -0400

    changed file type of voter turnout file

---
+++ clean_voterturnout.py
@@ -0,0 +1,17 @@
+"""
+Kimberly Stochaj
+DS2500
+Data cleaning for voter turnout
+2022 November 3
+clean_voterturnout.py
+"""
+FILENAME = 
+
+import pandas as pd
+
+
+def main():
+    
+    
+    
+main()
